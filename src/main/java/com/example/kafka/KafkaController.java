8f08420e7794a911cb2b9f9614a4903a52e2d8f5
test0008 <er.javatest@gmail.com>
2025-06-15 18:24:25 +0530

    Added logger changes.

--- src/main/java/com/example/kafka/KafkaController.java
+++ src/main/java/com/example/kafka/KafkaController.java
@@ -1,14 +1,21 @@
 package com.example.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.kafka.model.Employee;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/kafka")
+@Slf4j
 public class KafkaController {
 
+	private static final Logger log = LoggerFactory.getLogger(KafkaController.class);
+	
     @Autowired
     private ProducerService producerService;
 
@@ -20,6 +27,7 @@ public class KafkaController {
 
     @PostMapping("/employeePublish")
     public void sendEmployeeObject(@RequestBody Employee emp) {
+    	log.info("API called to send Employee Object via Kafka Topic.");
         producerService.sendEmployeeObject(emp);
     }
 }
